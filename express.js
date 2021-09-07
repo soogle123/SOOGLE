178b034ee51c0430b45dcf680e93a550505b4d55
abdul shihab <soogleb37@gmail.com>
2021-09-07 15:50:57 +0530

    soofle

--- express.js
+++ express.js
@@ -1,14 +1,25 @@
 
 const express = require('express')
+
 const app = express()
 const port = 3409
-
+const bodyparser=require('body-parser')
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/signup',(req, res) => {
-    res.send('get signup!')
+app.get('/sumbit',(req, res) => {
+    res.send('get sumbit')
+})
+
+app.post('/sumbit',(req, res) => {
+    console.log(req.body);
+    console.log(req.body.username);
+
+    res.send('get sumbit!')
+
 })
 app.get('/login',(req, res) => {
     res.send('get login!')
